e15f2b83abd6256c09cc6bab46fda45dc38f1d84
asleepace <colin_teahan@yahoo.com>
2020-07-27 15:45:45 -0700

    adding basic styling

--- example/App.js
+++ example/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import {initialize, endAppStartup} from 'react-native-embrace';
 
 export default class App extends React.Component {
@@ -15,6 +15,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     initialize();
+    console.log('[App] application started!');
   }
 
   componentDidMount() {
@@ -24,10 +25,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>{'Embrace + Detox Example'}</Text>
-        <Text style={styles.body}>
-          {'This is an example test application using embrace and detox.'}
-        </Text>
+        <View>
+          <Text style={styles.header}>{'Embrace + Detox Example'}</Text>
+          <Text style={styles.body}>
+            {'This is an example test application using embrace and detox.'}
+          </Text>
+          <Button title={'CLick me'} onPress={() => {}} />
+        </View>
       </View>
     );
   }
@@ -38,16 +42,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#eee',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
+    padding: 32,
     flex: 1,
   },
   header: {
+    textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 24,
-    padding: 8,
+    padding: 32,
   },
   body: {
     textAlign: 'center',
     fontSize: 16,
+    paddingBottom: 32,
   },
 });
